6d1226ef2bed3c7b12349d520e657616cb6053a0
Nirzon Pop <nirzon192@gmail.com>
2017-07-13 16:37:23 +0600

    Dues to missing of the  getResources().getDimensionPixelOffset() the previous problem occurs

--- app/src/main/java/com/learn/faisal/cascadelayout/view/CascadeLayout.java
+++ app/src/main/java/com/learn/faisal/cascadelayout/view/CascadeLayout.java
@@ -12,34 +12,45 @@ import com.learn.faisal.cascadelayout.R;
  * Created by TD-Android
  * on 7/12/2017.
  */
+
 public class CascadeLayout extends ViewGroup {
+
     private int mHorizontalSpacing;
     private int mVerticalSpacing;
 
-    public CascadeLayout(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
+    public CascadeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
 
-        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.CascadeLayout);
+        TypedArray a = context.obtainStyledAttributes(attrs,
+                R.styleable.CascadeLayout);
 
         try {
-            mHorizontalSpacing = ta.getDimensionPixelSize(R.styleable.CascadeLayout_horizontal_spacing
-                    , R.dimen.cascade_horizontal_spacing);
-            mVerticalSpacing = ta.getDimensionPixelSize(R.styleable.CascadeLayout_vertical_spacing,
-                    R.dimen.cascade_vertical_spacing);
+            mHorizontalSpacing = a.getDimensionPixelSize(
+                    R.styleable.CascadeLayout_horizontal_spacing,
+                    getResources().getDimensionPixelOffset(
+                            R.dimen.cascade_horizontal_spacing));
+
+            mVerticalSpacing = a.getDimensionPixelSize(
+                    R.styleable.CascadeLayout_vertical_spacing,
+                    getResources().getDimensionPixelSize(
+                            R.dimen.cascade_vertical_spacing));
         } finally {
-            ta.recycle();
+            a.recycle();
         }
 
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-//        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        int width = 0;
+
+        int width = getPaddingLeft();
         int height = getPaddingTop();
+
+        int verticalSpacing;
         final int childCount = getChildCount();
 
         for (int i = 0; i < childCount; i++) {
+            verticalSpacing = mVerticalSpacing;
             View child = getChildAt(i);
 
             measureChild(child, widthMeasureSpec, heightMeasureSpec);
@@ -49,63 +60,209 @@ public class CascadeLayout extends ViewGroup {
             lp.x = width;
             lp.y = height;
 
+            if (lp.verticalSpacing >= 0) {
+                verticalSpacing = lp.verticalSpacing;
+            }
+
             width += child.getMeasuredWidth();
-            height += mVerticalSpacing;
+            height += verticalSpacing;
 
         }
 
         width += getPaddingRight();
-        height += getChildAt(childCount - 1).getMeasuredHeight() + getPaddingBottom();
+        height += getChildAt(childCount - 1).getMeasuredHeight()
+                + getPaddingBottom();
 
-        setMeasuredDimension(resolveSize(width, widthMeasureSpec), resolveSize(height, heightMeasureSpec));
+        setMeasuredDimension(resolveSize(width, widthMeasureSpec),
+                resolveSize(height, heightMeasureSpec));
 
 
     }
 
     @Override
-    protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
-        final int childCount = getChildCount();
+    protected void onLayout(boolean changed, int l, int b, int t, int r) {
+        final int count = getChildCount();
 
-        for (int l = 0; l < childCount; l++) {
+        for (int i = 0; i < count; i++) {
 
             View child = getChildAt(i);
 
             LayoutParams lp = (LayoutParams) child.getLayoutParams();
-            child.layout(lp.x, lp.y, lp.x + child.getMeasuredWidth(), lp.y + child.getMeasuredHeight());
+            child.layout(lp.x, lp.y, lp.x + child.getMeasuredWidth(), lp.y
+                    + child.getMeasuredHeight());
+        }
+    }
+
+
+    @Override
+    protected boolean checkLayoutParams(ViewGroup.LayoutParams p) {
+        return p instanceof LayoutParams;
+    }
+
+    @Override
+    protected LayoutParams generateDefaultLayoutParams() {
+        return new LayoutParams(LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT);
+    }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new LayoutParams(getContext(), attrs);
+    }
+
+    @Override
+    protected LayoutParams generateLayoutParams(ViewGroup.LayoutParams p) {
+        return new LayoutParams(p.width, p.height);
+    }
+
+    public static class LayoutParams extends ViewGroup.LayoutParams {
+
+        int x;
+        int y;
+        public int verticalSpacing;
+
+        public LayoutParams(Context context, AttributeSet attrs) {
+            super(context, attrs);
+
+            TypedArray a = context.obtainStyledAttributes(attrs,
+                    R.styleable.CascadeLayout_LayoutParams);
+            try {
+                verticalSpacing = a
+                        .getDimensionPixelSize(
+                                R.styleable.CascadeLayout_LayoutParams_layout_vertical_spacing,
+                                -1);
+            } finally {
+                a.recycle();
+            }
+        }
+
+        public LayoutParams(int w, int h) {
+            super(w, h);
+        }
+    }
+}
+
+
+
+/*public class CascadeLayout extends ViewGroup {
+
+    private int mHorizontalSpacing;
+    private int mVerticalSpacing;
+
+    public CascadeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        TypedArray a = context.obtainStyledAttributes(attrs,
+                R.styleable.CascadeLayout);
+
+        try {
+            mHorizontalSpacing = a.getDimensionPixelSize(
+                    R.styleable.CascadeLayout_horizontal_spacing,
+                    getResources().getDimensionPixelSize(
+                            R.dimen.cascade_horizontal_spacing));
+
+            mVerticalSpacing = a.getDimensionPixelSize(
+                    R.styleable.CascadeLayout_vertical_spacing, getResources()
+                            .getDimensionPixelSize(R.dimen.cascade_vertical_spacing));
+        } finally {
+            a.recycle();
+        }
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = getPaddingLeft();
+        int height = getPaddingTop();
+        int verticalSpacing;
+
+        final int count = getChildCount();
+        for (int i = 0; i < count; i++) {
+            verticalSpacing = mVerticalSpacing;
+
+            View child = getChildAt(i);
+            measureChild(child, widthMeasureSpec, heightMeasureSpec);
+
+            LayoutParams lp = (LayoutParams) child.getLayoutParams();
+            width = getPaddingLeft() + mHorizontalSpacing * i;
+
+            lp.x = width;
+            lp.y = height;
+
+            if (lp.verticalSpacing >= 0) {
+                verticalSpacing = lp.verticalSpacing;
+            }
+
+            width += child.getMeasuredWidth();
+            height += verticalSpacing;
+        }
+
+        width += getPaddingRight();
+        height += getChildAt(getChildCount() - 1).getMeasuredHeight()
+                + getPaddingBottom();
+
+        setMeasuredDimension(resolveSize(width, widthMeasureSpec),
+                resolveSize(height, heightMeasureSpec));
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+        final int count = getChildCount();
+        for (int i = 0; i < count; i++) {
+            View child = getChildAt(i);
+            LayoutParams lp = (LayoutParams) child.getLayoutParams();
+
+            child.layout(lp.x, lp.y, lp.x + child.getMeasuredWidth(), lp.y
+                    + child.getMeasuredHeight());
         }
     }
 
     @Override
     protected boolean checkLayoutParams(ViewGroup.LayoutParams p) {
-//        return super.checkLayoutParams(p);
         return p instanceof LayoutParams;
     }
 
     @Override
-    public ViewGroup.LayoutParams generateLayoutParams(AttributeSet attrs) {
+    protected LayoutParams generateDefaultLayoutParams() {
+        return new LayoutParams(LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT);
+    }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
         return new LayoutParams(getContext(), attrs);
     }
 
     @Override
-    protected ViewGroup.LayoutParams generateLayoutParams(ViewGroup.LayoutParams p) {
+    protected LayoutParams generateLayoutParams(ViewGroup.LayoutParams p) {
         return new LayoutParams(p.width, p.height);
     }
 
-    @Override
-    protected ViewGroup.LayoutParams generateDefaultLayoutParams() {
-        return new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-    }
-
     public static class LayoutParams extends ViewGroup.LayoutParams {
+        int x;
+        int y;
+        public int verticalSpacing;
 
-        int x, y;
+        public LayoutParams(Context context, AttributeSet attrs) {
+            super(context, attrs);
 
-        public LayoutParams(Context c, AttributeSet attrs) {
-            super(c, attrs);
+            TypedArray a = context.obtainStyledAttributes(attrs,
+                    R.styleable.CascadeLayout_LayoutParams);
+            try {
+                verticalSpacing = a
+                        .getDimensionPixelSize(
+                                R.styleable.CascadeLayout_LayoutParams_layout_vertical_spacing,
+                                -1);
+            } finally {
+                a.recycle();
+            }
         }
 
-        public LayoutParams(int width, int height) {
-            super(width, height);
+        public LayoutParams(int w, int h) {
+            super(w, h);
         }
+
     }
-}
+}*/
+
